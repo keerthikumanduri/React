912a1e41068b3f52c41536b4f36b8422c127b1d1
keerthikumanduri <keerthivardhan2k@gmail.com>
2023-10-27 13:41:20 +0530

    Initial React setup content.

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Coffee from './Coffee'
+
+function App() {
+  return (
+    <>
+    <Coffee/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Coffee.js
@@ -0,0 +1,7 @@
+function Coffee(){
+    return(
+        <h2>Cafe coffeeday.</h2>
+    )
+}
+
+export default Coffee
